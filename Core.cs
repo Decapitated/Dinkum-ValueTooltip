8232f30780c34fe4775274efe86734f50f6502e5
Brett Johnson <brettzkyjo10@gmail.com>
2025-11-03 15:57:27 -0600

    Initial commit

---
+++ Core.cs
@@ -0,0 +1,15 @@
+﻿using MelonLoader;
+
+[assembly: MelonInfo(typeof(ValueTooltipMod.Core), "ValueTooltipMod", "1.0.0", "brett", null)]
+[assembly: MelonGame("James Bendon", "Dinkum")]
+
+namespace ValueTooltipMod
+{
+    public class Core : MelonMod
+    {
+        public override void OnInitializeMelon()
+        {
+            LoggerInstance.Msg("Initialized.");
+        }
+    }
+}
